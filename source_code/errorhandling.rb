cc3653328718f0de15f8431690e1efcd3f57734c
aes89 <adrienne.e.smith@outlook.com>
2020-04-26 15:49:28 +1000

    no functional changes

--- source_code/errorhandling.rb
+++ source_code/errorhandling.rb
@@ -1,4 +1,4 @@
-class InvalidName < StandardError
+class InvalidInput < StandardError
 end
 
 class FloatNotInt < StandardError
@@ -8,25 +8,26 @@ end
 #*** NAME
 def validate_name(name)
     name = name.strip
-    raise InvalidName, "That's not a name pal, try some letters." if name.empty?
+    raise InvalidInput, "That's not a name pal, try some letters." if name.empty?
     name
 end
 
 def user
     puts "What's your name, friend?"
-    @name = gets.strip
-    validate_name(@name)
-rescue InvalidName => e
+    name = gets.strip
+    validate_name(name)
+rescue InvalidInput => e
     puts "Invalid name, error details: #{e.message}"
     retry
 end
 
 
+
 #***GOAL
 
 def validate_goal(goal)
-    raise InvalidName, "You can do better than that, we need some numbers here." if goal.empty?
-    raise InvalidName, "Only numbers, thanks." if goal.to_i == 0 and goal != "0"
+    raise InvalidInput, "You can do better than that, we need some numbers here." if goal.empty?
+    raise InvalidInput, "Only numbers, thanks." if goal.to_i == 0 and goal != "0"
     raise FloatNotInt, "We're not splitting hairs here. Only whole numbers thanks." if (goal.to_f % 1) != 0
     goal.to_i
 end
@@ -35,11 +36,14 @@ def goal
     puts "How many points will you aim for?"
     goal = gets.strip
     validate_goal(goal)
-    rescue InvalidName => e 
+    rescue InvalidInput => e 
         puts "Invalid goal, error details: #{e.message}"
         retry
     rescue FloatNotInt => e
         puts "Invalid goal, error details: #{e.message}"
         retry
 end
+
+#*** TIME
+
 
