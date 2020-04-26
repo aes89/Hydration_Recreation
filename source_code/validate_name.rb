ce114a90e2bf0d8e753b5b156cf1d3c0c7d80470
aes89 <adrienne.e.smith@outlook.com>
2020-04-26 20:55:45 +1000

    file added for name validating method

---
+++ source_code/validate_name.rb
@@ -0,0 +1,11 @@
+class InvalidInput < StandardError
+end
+
+class FloatNotInt < StandardError
+end
+
+def validate_name(name)
+    name = name.strip
+    raise InvalidInput, "That's not a name pal, try some letters." if name.empty?
+    name
+end
