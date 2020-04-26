cd92376b4000fa1d8e84a49de566a840b0415990
aes89 <adrienne.e.smith@outlook.com>
2020-04-26 15:48:43 +1000

    user name removed - to be added again later

--- source_code/drinks_classes.rb
+++ source_code/drinks_classes.rb
@@ -1,3 +1,4 @@
+require 'tty-link'
 
 class DrinkPoints
     def initialize (amount, breakfastpoints, lunchpoints, dinnerpoints, breakfastMessage, lunchMessage, dinnerMessage)
@@ -11,23 +12,23 @@ class DrinkPoints
 
     def breakfast
         puts @breakfastMessage
-        @amount = @amount * @breakfastpoints
+        @amount * @breakfastpoints
     end
 
     def lunch
         puts @lunchMessage
-        @amount = @amount * @lunchpoints
+        @amount * @lunchpoints
     end
 
     def dinner
         puts @dinnerMessage
-        @amount = @amount * @dinnerpoints
+        @amount * @dinnerpoints
     end
 end
 
 class WaterPoints < DrinkPoints
     def initialize (amount)
-        super   @breakfastpoints = 125, @lunchpoints = 125, @dinnerpoints = 125, @amount = amount, @breakfastMessage, @lunchMessage, @dinnerMessage
+        super @amount = amount, @breakfastpoints = 125, @lunchpoints = 125, @dinnerpoints = 125, @breakfastMessage, @lunchMessage, @dinnerMessage
     end
 
     def permanent
@@ -38,31 +39,31 @@ end
 
 class JuicePoints < DrinkPoints
     def initialize (amount)
-        super @breakfastpoints = 110, @lunchpoints = 90, @dinnerpoints = 70, @breakfastMessage = "Juice is optimal morning drink!", @lunchMessage = "Not ideal, but I'll let it slide", @dinnerMessage = "Naughty naughty, less of that please", @amount = amount
+        super @amount = amount, @breakfastpoints = 110, @lunchpoints = 90, @dinnerpoints = 70, @breakfastMessage = "Juice is optimal morning drink!", @lunchMessage = "Not ideal, but I'll let it slide", @dinnerMessage = "Naughty naughty, less of that please"
     end
 end
 
 class CaffinatedPoints < DrinkPoints
     def initialize (amount)
-        super @breakfastpoints = 100, @lunchpoints = 50, @dinnerpoints = (-10), @amount = amount, @breakfastMessage = "Up and at 'em!", @lunchMessage = "Not ideal, but I'll let it slide", @dinnerMessage = "Uh oh, looks like you lost points here. Good liuck sleeping tonight #{@name}."
+        super @amount = amount, @breakfastpoints = 100, @lunchpoints = 50, @dinnerpoints = -10 , @breakfastMessage = "Up and at 'em!", @lunchMessage = "Not ideal, but I'll let it slide", @dinnerMessage = "Uh oh, looks like you lost points here. Good luck sleeping tonight."
     end
 end
 
 class AlcoholicPoints < DrinkPoints
     def initialize (amount)
-        super @breakfastpoints = (-50), @lunchpoints = 20, @dinnerpoints = (50), @amount = amount, @breakfastMessage = "In the morning? #{@name}, maybe visit this site: AA", @lunchMessage = "Not ideal, but I'll let it slide", @dinnerMessage = "Not needed but preferable in the evening."
+        super @amount = amount, @breakfastpoints = -50, @lunchpoints = 20, @dinnerpoints = 50, @breakfastMessage = "In the morning? Maybe visit this site: #{TTY::Link.link_to("Alcoholics Anonymous", "https://aa.org.au/")}", @lunchMessage = "Not ideal, but I'll let it slide", @dinnerMessage = "Not needed but preferable in the evening."
     end
 end
 
 class SodaPoints < DrinkPoints
     def initialize (amount)
-        super @breakfastpoints = (-10), @lunchpoints = 50, @dinnerpoints = 50, @amount = amount, @breakfastMessage = "In the morning? Give 10 points back. Sheesh.", @lunchMessage = "Not ideal, but I'll let it slide", @dinnerMessage = "Not ideal, but I'll let it slide"
+        super @amount = amount, @breakfastpoints = -10, @lunchpoints = 50, @dinnerpoints = 50, @breakfastMessage = "In the morning? Give 10 points back. Sheesh.", @lunchMessage = "Not ideal, but I'll let it slide", @dinnerMessage = "Not ideal, but I'll let it slide"
     end
 end
 
 class OtherPoints < DrinkPoints
     def initialize (amount)
-        super @breakfastpoints = 80, @lunchpoints = 80, @dinnerpoints = 80, @amount = amount, @breakfastMessage = "Not sure what this was #{@name}, you can have some points.", @lunchMessage = "NNot sure what this was #{@name}, you can have some points.", @dinnerMessage = "Not sure what this was #{@name}, you can have some points."
+        super @breakfastpoints = 80, @lunchpoints = 80, @dinnerpoints = 80, @amount = amount, @breakfastMessage = "Not sure what this was, you can have some points.", @lunchMessage = "Not sure what this was, you can have some points.", @dinnerMessage = "Not sure what this was, you can have some points."
     end
 end
 
